dff77cee39fc3dd19d5c88873e6f7b8f2cc57c78
PengchengLiu0909 <lpctjut@163.com>
2016-05-25 21:25:07 +0800

    update CameraUtils
    拍照工具类

--- app/src/main/java/com/example/lpc/faceidentify/CameraUtils.java
+++ app/src/main/java/com/example/lpc/faceidentify/CameraUtils.java
@@ -1,8 +1,14 @@
 package com.example.lpc.faceidentify;
 
+
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by lpc on 2016/5/25.
@@ -15,7 +21,8 @@ public class CameraUtils {
     Camera camera;
 
     private int imageWidth = Contants.IMAGE_WIDTH;
-
+    private int imageHeight = Contants.IMAGE_HEIGHT;
+    private int rate = Contants.RATE;
 
     public CameraUtils(SurfaceView surfaceView){
         this.surfaceView = surfaceView;
@@ -25,7 +32,71 @@ public class CameraUtils {
 
     //选择摄像头进行拍照
     public void switchCamera(int id){
+        mCurrentCamera = id;
+        int cameraCount = 0;
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        cameraCount = Camera.getNumberOfCameras();
+        for (int i =0;i < cameraCount ;i++){
+            Camera.getCameraInfo(i,cameraInfo);
+            if (mCurrentCamera == Contants.BACK_CAMERA){
+                if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK){
+                    camera = Camera.open(i);
+                }
+            }else
+            {
+                if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
+                    camera = Camera.open(i);   //打开前置摄像头
+                }
+            }
+        }
 
+        Camera.Parameters parameters = camera.getParameters() ;
+        List<Camera.Size> sizes = parameters.getSupportedPictureSizes();
+        //对分辨率进行排序
+        Collections.sort(sizes, new Comparator<Camera.Size>() {
+            @Override
+            public int compare(Camera.Size lhs, Camera.Size rhs) {
+                return lhs.width * lhs.height - rhs.width * rhs.height;
+            }
+        });
+
+        for (int i = 0;i<sizes.size();i++){
+            if (sizes.get(i).width >= imageWidth || sizes.get(i).height >= imageHeight || i == sizes.size() - 1){
+                imageWidth = sizes.get(i).width;
+                imageHeight = sizes.get(i).height;
+            }
+        }
+
+        parameters.setPictureSize(imageWidth,imageHeight);
+        parameters.setPreviewFrameRate(rate);
+        parameters.setJpegQuality(80);
+        camera.setParameters(parameters);
+
+        startPreview();
+    }
+
+    public void startPreview(){
+        if (camera != null){
+            try{
+                camera.setPreviewDisplay(surfaceHolder);
+                camera.startPreview();
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    //进行拍照
+    public void takePhoto(){
+        camera.takePicture(null,null,new MyTakePhotoCallback());
+    }
+
+    public final class MyTakePhotoCallback implements Camera.PictureCallback{
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+        }
     }
 
 }
